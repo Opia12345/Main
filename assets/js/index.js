f4cf20ca0d1514ed2a135f2910eb1ad789ca790d
Opia12345 <119626942+Opia12345@users.noreply.github.com>
2022-12-04 15:48:17 -0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+//Pop Up
+var popup= document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup")
+
+
+if(popup.classList.contains("open-popup"))
+   setTimeout(() => popup.classList.remove("open-popup"), 4000)
+}
+//...
+
+// Form Validations
+
+
+//...
+
+
+
